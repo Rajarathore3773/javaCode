5ee1fd284ca8f1b6322c0467a30bcc04ef7eccbf
Rajarathore3773 <104621878+Rajarathore3773@users.noreply.github.com>
2022-06-20 21:33:40 +0530

    Add files via upload

---
+++ enumPackege/DaysOfTheWeek.java
@@ -0,0 +1,8 @@
+package EnumPackage;
+
+public enum DaysOfTheWeek {
+
+	Monday,Tuesday,Wednesday,Thurseday;
+	
+
+}
---
+++ enumPackege/TestEnum.java
@@ -0,0 +1,11 @@
+package EnumPackage;
+
+public class TestEnum {
+	
+public static void main(String[] args) {
+	
+	
+	for(DaysOfTheWeek d: DaysOfTheWeek.values())
+		System.out.println(d);
+}	
+}
